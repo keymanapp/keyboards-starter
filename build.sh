b2ec73af59ffd418cbb23dfda92390481d30110c
Marc Durdin <marc@keyman.com>
2018-05-31 13:22:47 +0700

    Update build script to support keyboards-starter

--- build.sh
+++ build.sh
@@ -42,7 +42,18 @@ KEYBOARDINFO_SCHEMA_DIST_JSON="$KEYBOARDROOT/tools/keyboard_info.distribution.js
 parse_args "$@"
 
 #
+# Are we in the keyboards_starter repo?
 #
+
+if [[ -d "$KEYBOARDROOT/template" ]]; then
+  if [[ -d "$KEYBOARDROOT/release" ]]; then
+    die "This repo should not have both a /release/ folder and a /template/ folder. The /template/ folder should be present only in the keyboards_starter repo."
+  fi
+  KEYBOARDS_STARTER=1
+fi
+
+#
+# Select action
 #
 
 if [ "$DO_BUILD" = false ]; then
@@ -57,7 +68,12 @@ fi
 # Collect filenames
 #
 
-KEYBOARD_INFO_PATHS="$KEYBOARDROOT"/*/*/*/*.keyboard_info
+if [[ $KEYBOARDS_STARTER == 1 ]]; then
+  KEYBOARD_INFO_PATHS="$KEYBOARDROOT"/*/*/*/*/*.keyboard_info
+else
+  KEYBOARD_INFO_PATHS="$KEYBOARDROOT"/*/*/*/*.keyboard_info
+fi
+
 KEYBOARD_INFOS=($KEYBOARD_INFO_PATHS)
 KEYBOARD_INFOS=`printf -- '%s\n' "${KEYBOARD_INFOS[@]}"`
 
@@ -65,20 +81,28 @@ KEYBOARD_INFOS=`printf -- '%s\n' "${KEYBOARD_INFOS[@]}"`
 # Run build
 #
 
-if [[ "$TARGET" ]]; then
-  if [[ "$TARGET" == */* ]] && [[ (-d "$TARGET") ]]; then
-    group=$(cut -d / -f 1 <<< "$TARGET")
-    echo "--- Only building $group $TARGET ---"
-    build_keyboard $group "$TARGET"
-  elif [[ "$TARGET" == "release" ]] || [[ "$TARGET" == "legacy" ]] || [[ "$TARGET" == "experimental" ]]; then
-    # Assuming release|legacy|experimental
-    echo "--- Only building $TARGET ---"
-    build_keyboards "$TARGET"
+if [[ $KEYBOARDS_STARTER == 1 ]]; then
+  if [[ "$TARGET" ]]; then
+    build_keyboard "$TARGET"
   else
-    display_usage
+    build_keyboards
   fi
 else
-  build_keyboards release
-  build_keyboards legacy
-  build_keyboards experimental
+  if [[ "$TARGET" ]]; then
+    if [[ "$TARGET" == */* ]] && [[ (-d "$TARGET") ]]; then
+      group=$(cut -d / -f 1 <<< "$TARGET")
+      echo "--- Only building $group $TARGET ---"
+      build_keyboard $group "$TARGET"
+    elif [[ "$TARGET" == "release" ]] || [[ "$TARGET" == "legacy" ]] || [[ "$TARGET" == "experimental" ]]; then
+      # Assuming release|legacy|experimental
+      echo "--- Only building $TARGET ---"
+      build_keyboards "$TARGET"
+    else
+      display_usage
+    fi
+  else
+    build_keyboards release
+    build_keyboards legacy
+    build_keyboards experimental
+  fi
 fi
